ec24ca80e2dde1a136b475292741bd0cbf6d8c57
jyhyun1008 <jayhyun1008@gmail.com>
2025-12-28 17:17:48 +0900

    Updates

--- _nuxt/C7Gkrb9b.js
+++
@@ -1,1 +0,0 @@
-import{r as s}from"./BJjW5vzq.js";import t from"./BDOJqWAN.js";import{a0 as a,b as e,o as p,v as c,y as n}from"./DvNjvwnL.js";import"./BN_7HF1G.js";const d={__name:"ProseTip",setup(i){const o=a();return(r,f)=>(p(),e(t,{color:"success",icon:n(o).ui.icons.tip},{default:c(()=>[s(r.$slots,"default",{mdcUnwrap:"p"})]),_:3},8,["icon"]))}};export{d as default};
---
+++ _nuxt/D6-iXrY9.js
@@ -0,0 +1,1 @@
+import{r as a}from"./3T4RAHBt.js";import t from"./DMs5QyFl.js";import{a0 as e,b as n,o as s,v as c,y as i}from"./BTbwZ60Z.js";import"./BN_7HF1G.js";const d={__name:"ProseCaution",setup(p){const o=e();return(r,f)=>(s(),n(t,{color:"error",icon:i(o).ui.icons.caution},{default:c(()=>[a(r.$slots,"default",{mdcUnwrap:"p"})]),_:3},8,["icon"]))}};export{d as default};
