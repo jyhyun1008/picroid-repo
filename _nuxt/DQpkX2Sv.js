46f16180c23d540eb735d80a72e249853c8a6182
jyhyun1008 <jayhyun1008@gmail.com>
2025-12-28 15:51:32 +0900

    Updates

--- _nuxt/DQpkX2Sv.js
+++
@@ -1,1 +0,0 @@
-import{r as s}from"./CJ9_o63z.js";import t from"./CFS4qLhs.js";import{a0 as a,b as e,o as p,v as c,y as n}from"./DBmx4aXb.js";import"./BN_7HF1G.js";const d={__name:"ProseTip",setup(i){const o=a();return(r,f)=>(p(),e(t,{color:"success",icon:n(o).ui.icons.tip},{default:c(()=>[s(r.$slots,"default",{mdcUnwrap:"p"})]),_:3},8,["icon"]))}};export{d as default};
